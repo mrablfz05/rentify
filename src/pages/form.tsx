6ab62c25f5dc069fb17ebf86d898e0900a2e65fc
mrablfz <abolfaz88888888@gmail.com>
2025-04-16 02:10:49 -0700

    style: remove registration file

--- src/pages/form.tsx
+++
@@ -1,9 +0,0 @@
-import SignUp from "@/app/auth/SignUp";
-
-export default function form() {
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
-}
---
+++ src/pages/registration.tsx
@@ -0,0 +1,5 @@
+import SignUp from "@/app/auth/SignUp";
+
+export default function registration() {
+  return <div className="bg-red-700">text</div>;
+}
